08655c93db258b8d699ea059663be97bf7f54267
Ana <anmeuri@gmail.com>
2023-12-12 18:12:13 -0800

    Create obj from game class and starts game

--- main.rb
+++ main.rb
@@ -4,30 +4,14 @@ require './questions'
 
 puts "Welcome to TWO-O-Player!"
 puts
-puts "Player one will play first, followed by player two."
+puts "Player 1 will play first, followed by Player 2."
 puts
-puts "Make sure to take turns, first question for player 1, second question for player two!"
-
-P1 = Player.new
-P2 = Player.new
-question = Question.new
-
-game = Game.new(P1, P2, question)
-puts "Player 1, " + question.question
-print "> "
-answer = $stdin.gets.chomp.to_i
-
-if question.validate_answer(answer)
-  puts "That's correct!"
-  P1.add_point
-else
-  puts "Uh-oh! Incorrect!"
-  P1.lose_life
-end
-
+puts "Make sure to take turns, first question for Player 1, second question for Player 2!"
 puts
-puts "--- Lives ---"
-puts "P1: #{P1.lives}/3 vs P2: #{P2.lives}/3"
-puts
-  
 
+P1 = Player.new("Player 1")
+P2 = Player.new("Player 2")
+
+game = Game.new(P1, P2)
+
+game.game_loop
